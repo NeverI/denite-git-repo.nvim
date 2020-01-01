8afc64c35b2b87666a5d9e41e96f4f08bd65b71c
Kiss Gabor <nevi@agmen.hu>
2020-01-01 14:11:12 +0100

    fetch action added

--- rplugin/python3/denite/kind/repo.py
+++ rplugin/python3/denite/kind/repo.py
@@ -1,6 +1,7 @@
-from denite.base.kind import Base
 import os
-#from denite.source.repo import Repo
+import re
+from denite.base.kind import Base
+from denite.util import debug, error
 
 class Kind(Base):
     def __init__(self, vim):
@@ -8,8 +9,8 @@ class Kind(Base):
 
         self.name = 'gitrepo'
         self.default_action = 'open'
-        self.persist_actions = [ 'open' ]
-        self.redraw_actions = []
+        self.persist_actions = [ 'open', 'fetch' ]
+        self.redraw_actions = [ 'fetch']
 
     def action_open(self, context):
         for target in context['targets']:
@@ -21,3 +22,39 @@ class Kind(Base):
             self.vim.command('cd ' + repo.path)
             self.vim.command('Gstatus')
             self.vim.command(f"bdelete {bufnr}")
+
+    def action_fetch(self, context):
+        for target in context['targets']:
+            repoAction = RepoAction(target['action__repo'], self.vim)
+            repoAction.fetch()
+
+class RepoAction():
+    def __init__(self, repo, vim):
+        self.vim = vim
+        self.repo = repo
+
+    def fetch(self):
+        result = self.repo._runGit(['fetch'])
+        self.repo.actionInfo = 'Fetch: '
+
+        if result['exitCode']:
+            self.repo.actionInfo += 'Failed'
+            return
+
+        if len(result['stderr']) is 0:
+            self.repo.actionInfo += 'Nothing new'
+            return
+
+        news = []
+        for line in result['stderr']:
+            branchMatch = re.search(r'\s([\w\.\-_]+)\s+->', line)
+            if not branchMatch:
+                continue
+
+            branch = branchMatch.group(1)
+            if re.search(r'\s\[new', line):
+                branch += '*'
+
+            news.append(branch)
+
+        self.repo.actionInfo += ', '.join(news)
